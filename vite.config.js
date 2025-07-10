59df6767e61ca8f203481714a6df1873263a3b4b
fcuadra-99 <fcuadra_230000001491@uic.edu.ph>
2025-07-10 20:03:21 +0800

    Version 0.2
    Patch Notes:
    - Converted vite-react project to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: [],
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
