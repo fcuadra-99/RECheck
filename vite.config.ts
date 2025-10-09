e7c361dbb1b2c0fb340aa6258bd49e3141872740
fcuadra-99 <176453547+fcuadra-99@users.noreply.github.com>
2025-10-09 18:08:23 +0800

    sdfsdf

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
       srcDir: "src",
       filename: "sw.js",
