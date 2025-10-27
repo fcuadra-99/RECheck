c432f1fd0cb6b42eebc08cc29289fee277fd9cc7
fcuadra-99 <176453547+fcuadra-99@users.noreply.github.com>
2025-10-27 10:41:52 +0800

    fix

--- src/pages/researcher/pcontent.tsx
+++ src/pages/researcher/pcontent.tsx
@@ -1589,11 +1589,11 @@ const getFormsDocuments = (submission: Submission): DocumentItem[] => {
         }
         return [
             makeDoc("REC_FO_0032_EthicsProtocolChecklist.pdf"),
-            makeDoc("REC_FO_0027_Ethics Application Procedure.pdf"),
-            makeDoc("REC_FO_0028_Ethics Study Protocol Information Form.pdf"),
-            makeDoc("REC_FO_0029_Ethics Informed Consent CHECKLIST.pdf"),
-            makeDoc("REC_FO_0030_Ethics Informed Consent Form when Questionnaire are Used.pdf"),
-            makeDoc("REC_FO_0031_Ethics Informed Consent Form (ICF)_Sample.pdf"),
+            makeDoc("REC_FO_0027_EthicsApplicationProcedure.pdf"),
+            makeDoc("REC_FO_0028_EthicsStudy ProtocolInformationForm.pdf"),
+            makeDoc("REC_FO_0029_EthicsInformedConsentCHECKLIST.pdf"),
+            makeDoc("REC_FO_0030_EthicsInformedConsentFormwhenQuestionnaireareUsed.pdf"),
+            makeDoc("REC_FO_0031_EthicsInformedConsentForm(ICF)_Sample.pdf"),
             makeDoc("REC_FO_0034_Ethics-Assent-Form-18-below-respondents_Sample.pdf"),
             makeDoc("REC_FO_0036_MOA for external.pdf"),
             ...commonFormsDocs,
