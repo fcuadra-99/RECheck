6b2c7ee88e55fc013d6d2523f9d15e7a81ce3e3f
fcuadra-99 <176453547+fcuadra-99@users.noreply.github.com>
2025-10-28 23:49:39 +0800

    awaw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nqem19369gg"
+    "revision": "0.jnojv3vr92o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
