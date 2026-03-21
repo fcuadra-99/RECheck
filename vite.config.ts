ef48d67ee7218ce4d37d942b1554360d931906f9
iko <franzilator9@gmail.com>
2026-03-21 20:02:38 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
       },
       srcDir: "src",
       filename: "sw.js",
